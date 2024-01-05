187eaf98bb274073a86c106cb26fe0c30c153614
nitinpawar-swapnavat <nitin.pawar@ilyticstech.com>
2024-01-05 19:17:07 +0530

    Lint Error fixed

--- app/app/tests.py
+++ app/app/tests.py
@@ -2,8 +2,8 @@
 Sample Tests
 """
 from django.test import SimpleTestCase
-
 from app import calc
+
 
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
@@ -11,10 +11,10 @@ class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
         """Test Adding Numbers together."""
         res = calc.add(5, 6)
-
         self.assertEqual(res, 11)
 
-    def test_substract_numbers(self):    
+
+    def test_substract_numbers(self):
+        """Test Substraction"""
         res = calc.substract(10, 15)
         self.assertEqual(res, -5)
-        
