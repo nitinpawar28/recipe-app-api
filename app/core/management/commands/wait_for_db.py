1793738ebdd344329f3acc5516e5c9243cde5a13
nitinpawar-swapnavat <nitin.pawar@ilyticstech.com>
2024-01-05 23:45:14 +0530

    docker wait for db

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,25 @@
+"""
+Django Command to wait for the database to be available
+"""
+import time
+from psycopg2 import OperationalError as Psycopg2OpError
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Django command to wait for database."""
+
+    def handle(self, *args, **options):
+        """EntryPoint for Command"""
+        self.stdout.write('Waiting for database...')
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except(Psycopg2OpError, OperationalError):
+                self.stdout.write('Database Not available, waiting...')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database is available.'))
